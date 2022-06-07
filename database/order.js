aef0b46deac37583d1c8b9947de99dc33b21558e
mostafa eltawel <mostafaeltawel123@gmail.com>
2022-06-07 22:52:14 +0200

    mostafa ismail

--- database/order.js
+++ database/order.js
@@ -1,59 +1,18 @@
-let order = require('../models/order');
-let createOrder=("/", verifyToken, async (req, res) => {
-    let newOrder = new Order(req.body);
-  
-    try {
-      let savedOrder = await newOrder.save();
-      res.status(200).json(savedOrder);
-    } catch (err) {
-      res.status(500).json(err);
+let mongoose = require('mongoose');
+let OrderSchema = new mongoose.Schema(
+    {
+userId :{type :String , required:true},
+products:[
+    { 
+        productId:{ type:  string},
+        quantity:{type:Number,default:1},
     }
-  });
-  let updatedOrder = ("/:id", verifyTokenAndAdmin, async (req, res) => {
-    try {
-      let updatedOrder = await Order.findByIdAndUpdate(
-        req.params.id,
-        {
-          $set: req.body,
-        },
-        { new: true }
-      );
-      res.status(200).json(updatedOrder);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
- let deleteOrder=("/:id", verifyTokenAndAdmin, async (req, res) => {
-    try {
-      await Order.findByIdAndDelete(req.params.id);
-      res.status(200).json("Order has been deleted...");
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-  let getUserOrder=("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
-    try {
-      let orders = await Order.find({ userId: req.params.userId });
-      res.status(200).json(orders);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-  let getAllOrder=("/", verifyTokenAndAdmin, async (req, res) => {
-    try {
-      let orders = await Order.find();
-      res.status(200).json(orders);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-  module.exports={
-    createOrder,
-    updatedOrder,
-    deleteOrder,
-    getUserOrder,
-    getAllOrder
-  }
+],
+amount:{type:Number,required:true},
+address:{type:object,required:true },
+status:{type:string, default:"panding"},
 
-  
-
+    },
+    {timestamps:true}
+);
+module.exports = OrderSchema;
